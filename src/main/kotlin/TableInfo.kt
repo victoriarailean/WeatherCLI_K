c2d851e3ca88ec6b028dead37263d004c8a2df2a
railean victoria <victoriarailean1998@gmail.com>
2025-08-31 14:17:40 +0300

    Created a new function to find the forecast day

--- src/main/kotlin/TableInfo.kt
+++ src/main/kotlin/TableInfo.kt
@@ -1,6 +1,8 @@
 import models.ApiResponse
 import models.Forecastday
 import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.ZoneId
 
 class TableInfo {
 
@@ -22,22 +24,33 @@ class TableInfo {
         var dateFormat = SimpleDateFormat("yyyy-MM-dd");
 
         for (c in apiResponseList) {
-            if (c.forecast != null && c.forecast.forecastday.size > 1) {
-                var forecastday: Forecastday = c.forecast.forecastday.get(1);
+            c.forecast?.let { forecast ->
+                val forecastday = getTomorrowForecast(forecast.forecastday)
+                forecastday?.let { fd ->
 
-                System.out.printf(
-                    "│ %-13s │ %-11s │ %-11s │ %-10s │ %-12s │ %-14s │ %-12s | %n",
-                    c.location.name,
-                    forecastday.day.maxtemp_c,
-                    forecastday.day.mintemp_c,
-                    forecastday.day.avghumidity,
-                    forecastday.day.maxwind_kph,
-                    "SSW",
-                    dateFormat.format(forecastday.date)
-                );
+                    System.out.printf(
+                        "│ %-13s │ %-11s │ %-11s │ %-10s │ %-12s │ %-14s │ %-12s | %n",
+                        c.location.name,
+                        fd.day.maxtemp_c,
+                        fd.day.mintemp_c,
+                        fd.day.avghumidity,
+                        fd.day.maxwind_kph,
+                        "SSW",
+                        dateFormat.format(fd.date)
+                    );
 
+                }
+                println("─────────────────────────────────────────────────────────────────────────────────────────────────────────")
             }
-            println("─────────────────────────────────────────────────────────────────────────────────────────────────────────")
+        }
+    }
+
+    private fun getTomorrowForecast(forecastList: List<Forecastday>): Forecastday? {
+        val today = LocalDate.now()
+        val tomorrow = today.plusDays(1)
+
+        return forecastList.find {
+            it.date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate() == tomorrow
         }
     }
 }
